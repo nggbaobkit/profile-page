3268c4632189c5634891e1796e7118924fa0bb99
Gia Bao <nggbaobkit@gmail.com>
2020-04-02 13:04:12 +0700

    Updates

---
+++ precache-manifest.9f2ab04da28e03968529a459628242eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0ffb6064add6947b3ce459feed89514",
+    "url": "/profile-page/index.html"
+  },
+  {
+    "revision": "ea478cca06622f5234ea",
+    "url": "/profile-page/static/css/main.7b4033f6.chunk.css"
+  },
+  {
+    "revision": "353b1ed60a720596a38b",
+    "url": "/profile-page/static/js/2.66a0e250.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/profile-page/static/js/2.66a0e250.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea478cca06622f5234ea",
+    "url": "/profile-page/static/js/main.bce8a746.chunk.js"
+  },
+  {
+    "revision": "2f2996a5065e983f8aec",
+    "url": "/profile-page/static/js/runtime-main.f80712be.js"
+  },
+  {
+    "revision": "cfc1e45145ebacc1a56fb5fb70a7aae0",
+    "url": "/profile-page/static/media/profilePic.cfc1e451.jpeg"
+  }
+]);
